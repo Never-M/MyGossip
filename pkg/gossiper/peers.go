49ac3cecf41d51b2e148ebbc804ebeb29d1a42da
Fuyao <fuyao@bu.edu>
2020-07-08 22:18:04 -0400

    Added heartbeat test. Added result code types

--- pkg/gossiper/peers.go
+++ pkg/gossiper/peers.go
@@ -3,6 +3,7 @@ package gossiper
 import (
 	"context"
 	"fmt"
+	. "github.com/Never-M/MyGossip/pkg/types"
 	"log"
 	"net/http"
 	"time"
@@ -10,11 +11,13 @@ import (
 
 // Time formatter
 var timeFormat = "2006-01-02 15:04:05"
+const HEARTBEAT_PORT = ":9000"
+const HEARTBEAT_CTXKEY = "heartbeat"
 
 type gossiper struct {
 	name	string
 	ip		string
-	peers	map[string]gossiper
+	peers	map[string]*gossiper
 	client  *http.Client
 }
 
@@ -23,22 +26,26 @@ type heartBeat struct {
 	currentTime		time.Time
 }
 
-func NewGossiper(self string, address string) *gossiper {
-	g := &gossiper{
-		name: self,
+func NewGossiper(name, address string) *gossiper {
+	return &gossiper{
+		name: name,
 		ip:address,
-		peers:make(map[string]gossiper),
+		peers:make(map[string]*gossiper),
 		client:&http.Client{},
 	}
-	return g;
 }
 
-func (g *gossiper) AddPeer(peer gossiper)  {
+func (g *gossiper) AddPeer(peer *gossiper) int {
+	if _, ok := g.peers[peer.name]; ok {
+		return OPERATION_ERROR
+	}
 	g.peers[peer.name] = peer
+	return SUCCEED
 }
 
-func (g *gossiper) RemovePeer(name string)  {
+func (g *gossiper) RemovePeer(name string) int {
 	delete(g.peers, name)
+	return SUCCEED
 }
 
 func (g *gossiper) PrintPeerNames() {
@@ -46,41 +53,47 @@ func (g *gossiper) PrintPeerNames() {
 }
 
 func (g *gossiper) HeartBeatHandler(w http.ResponseWriter, r *http.Request) {
-	hb := r.Context().Value("heartbeat")
+	hb := r.Context().Value(HEARTBEAT_CTXKEY)
 	hbStr := fmt.Sprint(hb)
-	log.Println("!!!" + hbStr)
+	log.Println(hbStr)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(hbStr))
 }
 
-func (g *gossiper) HeartBeatContext(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.WithValue(r.Context(), "heartbeat", heartBeat{g.name, time.Now()})
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
+// maybe the host doesn't need context
+//func (g *gossiper) HeartBeatContext(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		ctx := context.WithValue(r.Context(), HEARTBEAT_CTXKEY, heartBeat{g.name, time.Now()})
+//		next.ServeHTTP(w, r.WithContext(ctx))
+//	})
+//}
 
-func (g *gossiper) SendHeartBeat() {
+func (g *gossiper) SendHeartBeat() int {
 	ctx := context.WithValue(context.Background(), "heartbeat", &heartBeat{g.name, time.Now()})
-	ctx = context.WithValue(ctx, "key", "value")
-	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8085/heartbeat", nil)
-	if err != nil {
-		log.Println(err)
+	for _, peer := range g.peers {
+		req, err := http.NewRequestWithContext(ctx, "GET", "http://" + peer.ip + HEARTBEAT_PORT +"/" + HEARTBEAT_CTXKEY, nil)
+		if err != nil {
+			log.Println(err)
+			return HEARTBEAT_REQUEST_ERROR
+		}
+		_, err = g.client.Do(req)
+		if err != nil {
+			log.Println(err)
+			return HEARTBEAT_RESPONSE_ERROR
+		}
 	}
-	_, err = g.client.Do(req)
-	if err != nil {
-		log.Println(err)
-	}
+	return SUCCEED
 }
 
 func (g *gossiper) HeartBeatReceiver() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/heartbeat", g.HeartBeatHandler)
-	contextedMux := g.HeartBeatContext(mux)
-	log.Println("Start server on port :8085")
-	http.ListenAndServe(":8085", contextedMux)
+	// maybe the host doesn't need context
+	//contextedMux := g.HeartBeatContext(mux)
+	log.Println("Start server on " + g.ip + HEARTBEAT_PORT)
+	http.ListenAndServe(HEARTBEAT_PORT, nil)
 }
 
 func (hb heartBeat) String() string {
-	return "HeartBeat from: " + hb.gossiperName + ", at " + hb.currentTime.Format(timeFormat);
+	return "HeartBeat back from: " + hb.gossiperName + ", at " + hb.currentTime.Format(timeFormat)
 }
--- pkg/gossiper/peers_test.go
+++ pkg/gossiper/peers_test.go
@@ -1,16 +1,28 @@
 package gossiper
 
-import "testing"
+import (
+	. "github.com/Never-M/MyGossip/pkg/types"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
 
 func TestAddRemovePeer(t *testing.T)  {
-	g := new(gossiper)
-	g.name = "test"
-	g.AddPeer(gossiper{"peer", "localhost", make(map[string]gossiper)})
-	if _, ok := g.peers["peer"]; !ok {
-		t.Errorf("Add Failed")
-	}
-	g.RemovePeer("peer")
-	if _, ok := g.peers["peer"]; ok {
-		t.Errorf("Remove Failed")
-	}
+	assert := assert.New(t)
+	g := NewGossiper("node1", "localhost")
+	resultCode := g.AddPeer(NewGossiper("node2", "localhost"))
+	assert.Equal(SUCCEED, resultCode, "Add peer failed")
+
+	resultCode = g.RemovePeer("node2")
+	assert.Equal(SUCCEED, resultCode, "Remove peer failed")
+}
+
+func TestSendHeartBeat(t *testing.T)  {
+	assert := assert.New(t)
+	node1 := NewGossiper("node1", "localhost")
+	node2 := NewGossiper("node2", "localhost")
+	node1.AddPeer(node2)
+	go node2.HeartBeatReceiver()
+	resultCode := node1.SendHeartBeat()
+	assert.Equal(SUCCEED, resultCode, "Send heartbeat failed")
 }
